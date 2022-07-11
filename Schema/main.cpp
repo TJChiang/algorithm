d61fcb4e4d606ef833f117febaf449250b2f22b2
Nature <tjchiang8419@gmail.com>
2022-07-11 22:46:33 +0800

    新增 cpp 範例

---
+++ Schema/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+    public:
+        void func1() {
+            return;
+        }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution sol;
+    sol.func1();
+
+    return 0;
+}
