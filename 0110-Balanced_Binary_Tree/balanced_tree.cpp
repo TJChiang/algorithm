bc09a6e36dd8e20f2eebcd73be9d97feb64ab93d
tjchiang <tjchiang8419@gmail.com>
2023-08-03 18:07:12 +0800

    110. Balanced Binary Tree

---
+++ 0110-Balanced_Binary_Tree/balanced_tree.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include "../structures/tree_node.h"
+#include "../structures/binary_tree.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int getHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int left = getHeight(node->left);
+        if (left == -1) return -1;
+        int right = getHeight(node->right);
+        if (right == -1) return -1;
+
+        return abs(left - right) > 1 ? -1 : 1 + max(left, right);
+    }
+    bool isBalanced(TreeNode* root) {
+        return getHeight(root) != -1;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<BinaryTree> questions = {
+            BinaryTree(vector<int> {3, 9, 20, INT_NULL_, INT_NULL_, 15, 7}),
+            BinaryTree(vector<int> {1, 2, 2, 3, 3, INT_NULL_, INT_NULL_, 4, 4}),
+            BinaryTree(vector<int> {}),
+    };
+
+    for (auto question : questions) {
+        cout << sol.isBalanced(question.get_root()) << endl;
+    }
+
+    return 0;
+}
