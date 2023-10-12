1abb7ad4569a7e09096660dc82b9cdb08f48769d
TJChiang <tjchiang8419@gmail.com>
2023-10-12 21:42:05 +0800

    0557 another solution

--- 0557-Reverse_Words_in_a_String_III/reverse_words_III.cpp
+++ 0557-Reverse_Words_in_a_String_III/reverse_words_III.cpp
@@ -28,6 +28,28 @@ public:
         }
         return result;
     }
+
+    string reverseWords2(string s) {
+        int start = 0;
+        int end = 0;
+        int size = s.length();
+
+        while (end < size) {
+            while (end < size && s[end] != ' ') {
+                end++;
+            }
+
+            reverse(s.begin() + start, s.begin() + end);
+
+            while (end < size && s[end] == ' ') {
+                end++;
+            }
+
+            start = end;
+        }
+
+        return s;
+    }
 };
 
 int main(int argc, char* argv[])
@@ -36,8 +58,10 @@ int main(int argc, char* argv[])
     string s1 = "Let's take LeetCode contest";
     string s2 = "God Ding";
     string result = sol.reverseWords(s1);
+    string result2 = sol.reverseWords2(s2);
 
     cout << result << endl;
+    cout << result2 << endl;
 
     return 0;
 }
