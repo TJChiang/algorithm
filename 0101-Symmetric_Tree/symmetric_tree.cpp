f7ffad8ed2789e5bb2bda06f484127a1dd5266ee
tjchiang <tjchiang8419@gmail.com>
2024-03-14 17:56:40 +0800

    101 C++ 改寫

---
+++ 0101-Symmetric_Tree/symmetric_tree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "../structures/tree_node.h"
+#include "../structures/binary_tree.h"
+
+using namespace std;
+
+class Solution {
+private:
+    bool compare(TreeNode const* left, TreeNode const* right) {
+        if (left == nullptr && right != nullptr) return false;
+        else if (left != nullptr && right == nullptr) return false;
+        else if (left == nullptr && right == nullptr) return true;
+        else if (left->val != right->val) return false;
+
+        return compare(left->right, right->left) && compare(left->left, right->right);
+    }
+public:
+    bool isSymmetric(TreeNode const* root) {
+        if (root == nullptr) return true;
+        return compare(root->left, root->right);
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution sol;
+    BinaryTree ex1(vector<int> {1,2,2,3,4,4,3});
+    bool const result1 = sol.isSymmetric(ex1.get_root());
+    BinaryTree ex2(vector<int> {1,2,2, INT_NULL_,3,INT_NULL_,3});
+    bool const result2 = sol.isSymmetric(ex2.get_root());
+
+    cout << "result1: ";
+    if (result1) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    cout << "result2: ";
+    if (result2) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    return 0;
+}
