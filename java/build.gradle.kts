f91c64e13386b129b67ba3788d2b834e55245a44
TJChiang <tjchiang8419@gmail.com>
2026-01-04 15:06:01 +0800

    [Java] Add Java formatter configuration and update Gradle settings for compatibility

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -5,14 +5,28 @@ plugins {
 group = "tjchiang"
 version = "1.0"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
